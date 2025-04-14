61982b65347a42395745a906db75ab34b9fd0018
y0ngw00 <woodylyw@snu.ac.kr>
2025-04-14 17:15:37 +0900

    Motion inpainting with external input motion code

--- sample/generate.py
+++ sample/generate.py
@@ -232,6 +232,263 @@ def main():
 
     abs_path = os.path.abspath(out_path)
     print(f'[Done] Results are at [{abs_path}]') 
+    
+def generate_upper_body_fixed_sequences():
+    args = generate_args()
+    fixseed(args.seed)
+    out_path = args.output_dir
+    name = os.path.basename(os.path.dirname(args.model_path))
+    niter = os.path.basename(args.model_path).replace('model', '').replace('.pt', '')
+    max_frames = 196 if args.dataset in ['kit', 'humanml'] else 60
+    fps = 12.5 if args.dataset == 'kit' else 20
+    n_frames = min(max_frames, int(args.motion_length*fps))
+    is_using_data = not any([args.text_prompt])
+    dist_util.setup_dist(args.device)
+    if out_path == '':
+        out_path = os.path.join(os.path.dirname(args.model_path),
+                                'samples_{}_{}_seed{}'.format(name, niter, args.seed))
+        if args.text_prompt != '':
+            out_path += '_' + args.text_prompt.replace(' ', '_').replace('.', '')
+
+    hints = None
+    # this block must be called BEFORE the dataset is loaded
+    if args.text_prompt != '':
+        if args.text_prompt == 'predefined':
+            # generate hint and text
+            texts, hints = collate_all(n_frames, args.dataset)
+            args.num_samples = len(texts)
+            if args.cond_mode == 'only_spatial':
+                # only with spatial control signal, and the spatial control signal is defined in utils/text_control_example.py
+                texts = ['' for i in texts]
+            elif args.cond_mode == 'only_text':
+                # only with text prompt, and the text prompt is defined in utils/text_control_example.py
+                hints = None
+        else:
+            # otherwise we use text_prompt
+            texts = [args.text_prompt]
+            args.num_samples = 1
+            hint = None
+
+    assert args.num_samples <= args.batch_size, \
+        f'Please either increase batch_size({args.batch_size}) or reduce num_samples({args.num_samples})'
+    # So why do we need this check? In order to protect GPU from a memory overload in the following line.
+    # If your GPU can handle batch size larger then default, you can specify it through --batch_size flag.
+    # If it doesn't, and you still want to sample more prompts, run this script with different seeds
+    # (specify through the --seed flag)
+    args.batch_size = args.num_samples  # Sampling a single batch from the testset, with exactly args.num_samples
+
+    print('Loading dataset...')
+    data = load_dataset(args, max_frames, n_frames)
+    total_num_samples = args.num_samples * args.num_repetitions
+
+    print("Creating model and diffusion...")
+    model, diffusion = create_model_and_diffusion(args, data)
+
+    print(f"Loading checkpoints from [{args.model_path}]...")
+    state_dict = torch.load(args.model_path, map_location='cpu')
+    load_model_wo_clip(model, state_dict)
+
+    if args.guidance_param != 1:
+        model = ClassifierFreeSampleModel(model)   # wrapping model with the classifier-free sampler
+    model.to(dist_util.dev())
+    model.eval()  # disable random masking
+
+    collate_args = [{'inp': torch.zeros(n_frames), 'tokens': None, 'lengths': n_frames}] * args.num_samples
+    # t2m
+    collate_args = [dict(arg, text=txt) for arg, txt in zip(collate_args, texts)]
+    if hints is not None:
+        collate_args = [dict(arg, hint=hint) for arg, hint in zip(collate_args, hints)]
+
+    _, model_kwargs = collate(collate_args)
+
+    #Load Input motion and normalize it.
+    # input_motions_double = np.load('dataset/new_joints_vec/userstudy_0_converted.npy', allow_pickle=True)
+    input_motions_double = np.load(args.input_motion, allow_pickle=True)
+    input_motions_double = torch.from_numpy(input_motions_double).unsqueeze(0).unsqueeze(-1) # [1, 263, 1, frames]
+    input_motions = input_motions_double.to(torch.float32)
+    input_motions = input_motions.permute(0, 2, 3, 1) # [1, 263, 1, frames]
+    if input_motions.shape[3] > n_frames:
+        input_motions = input_motions[:, :, :, :n_frames]
+    
+    spatial_norm_path = './dataset/humanml_spatial_norm'
+    mean = torch.from_numpy(data.dataset.t2m_dataset.mean).unsqueeze(0).unsqueeze(-1).unsqueeze(-1)
+    std = torch.from_numpy(data.dataset.t2m_dataset.std).unsqueeze(0).unsqueeze(-1).unsqueeze(-1)
+    input_motions = (input_motions - mean) / std
+    input_motions = input_motions.to(dist_util.dev())
+    
+    # add inpainting mask according to args
+    gt_frames_per_sample = {}
+    if args.edit_mode == 'upper_body':
+        model_kwargs['y']['inpainted_motion'] = input_motions
+    if args.edit_mode == 'in_between':
+        model_kwargs['y']['inpainting_mask'] = torch.ones_like(input_motions, dtype=torch.bool,
+                                                               device=input_motions.device)  # True means use gt motion
+        for i, length in enumerate(model_kwargs['y']['lengths'].cpu().numpy()):
+            start_idx, end_idx = int(args.prefix_end * length), int(args.suffix_start * length)
+            gt_frames_per_sample[i] = list(range(0, start_idx)) + list(range(end_idx, max_frames))
+            model_kwargs['y']['inpainting_mask'][i, :, :,
+            start_idx: end_idx] = False  # do inpainting in those frames
+    elif args.edit_mode == 'upper_body':
+        model_kwargs['y']['inpainting_mask'] = torch.tensor(humanml_utils.HML_UPPER_BODY_MASK, dtype=torch.bool,
+                                                            device=input_motions.device)  # True is lower body data
+        model_kwargs['y']['inpainting_mask'] = model_kwargs['y']['inpainting_mask'].unsqueeze(0).unsqueeze(
+            -1).unsqueeze(-1).repeat(input_motions.shape[0], 1, input_motions.shape[2], input_motions.shape[3])
+
+    for k, v in model_kwargs['y'].items():
+        if torch.is_tensor(v):
+            model_kwargs['y'][k] = v.to(dist_util.dev())
+
+    original_motions = []
+    all_motions = []
+    all_lengths = []
+    all_text = []
+    all_hint = []
+    all_hint_for_vis = []
+
+    for rep_i in range(args.num_repetitions):
+        print(f'### Sampling [repetitions #{rep_i}]')
+
+        # add CFG scale to batch
+        if args.guidance_param != 1:
+            model_kwargs['y']['scale'] = torch.ones(args.batch_size, device=dist_util.dev()) * args.guidance_param
+
+        sample_fn = diffusion.p_sample_loop
+
+        sample = sample_fn(
+            model,
+            (args.batch_size, model.njoints, model.nfeats, n_frames),
+            clip_denoised=False,
+            model_kwargs=model_kwargs,
+            skip_timesteps=0,  # 0 is the default value - i.e. don't skip any step
+            init_image=None,
+            progress=True,
+            dump_steps=None,
+            noise=None,
+            const_noise=False,
+        )
+
+        sample = sample[:, :263]
+        # Recover XYZ *positions* from HumanML3D vector representation
+        if model.data_rep == 'hml_vec':
+            n_joints = 22 if sample.shape[1] == 263 else 21
+            sample = data.dataset.t2m_dataset.inv_transform(sample.cpu().permute(0, 2, 3, 1)).float()
+            sample = recover_from_ric(sample, n_joints)
+            sample = sample.view(-1, *sample.shape[2:]).permute(0, 2, 3, 1)
+
+        rot2xyz_pose_rep = 'xyz' if model.data_rep in ['xyz', 'hml_vec'] else model.data_rep
+        rot2xyz_mask = None if rot2xyz_pose_rep == 'xyz' else model_kwargs['y']['mask'].reshape(args.batch_size, n_frames).bool()
+        sample = model.rot2xyz(x=sample, mask=rot2xyz_mask, pose_rep=rot2xyz_pose_rep, glob=True, translation=True,
+                               jointstype='smpl', vertstrans=True, betas=None, beta=0, glob_rot=None,
+                               get_rotations_back=False)
+
+        if args.unconstrained:
+            all_text += ['unconstrained'] * args.num_samples
+        else:
+            text_key = 'text' if 'text' in model_kwargs['y'] else 'action_text'
+            all_text += model_kwargs['y'][text_key]
+
+            if 'hint' in model_kwargs['y']:
+                hint = model_kwargs['y']['hint']
+                # denormalize hint
+                if args.dataset == 'humanml':
+                    spatial_norm_path = './dataset/humanml_spatial_norm'
+                elif args.dataset == 'kit':
+                    spatial_norm_path = './dataset/kit_spatial_norm'
+                else:
+                    raise NotImplementedError('unknown dataset')
+                raw_mean = torch.from_numpy(np.load(pjoin(spatial_norm_path, 'Mean_raw.npy'))).cuda()
+                raw_std = torch.from_numpy(np.load(pjoin(spatial_norm_path, 'Std_raw.npy'))).cuda()
+                mask = hint.view(hint.shape[0], hint.shape[1], n_joints, 3).sum(-1) != 0
+                hint = hint * raw_std + raw_mean
+                hint = hint.view(hint.shape[0], hint.shape[1], n_joints, 3) * mask.unsqueeze(-1)
+                hint = hint.view(hint.shape[0], hint.shape[1], -1)
+                # ---
+                all_hint.append(hint.data.cpu().numpy())
+                hint = hint.view(hint.shape[0], hint.shape[1], n_joints, 3)
+                all_hint_for_vis.append(hint.data.cpu().numpy())
+                
+
+
+        all_motions.append(sample.cpu().numpy().copy())
+
+        original_motion = model_kwargs['y']['inpainted_motion']
+        if model.data_rep == 'hml_vec':
+            n_joints = 22 if original_motion.shape[1] == 263 else 21
+            original_motion = data.dataset.t2m_dataset.inv_transform(original_motion.cpu().permute(0, 2, 3, 1)).float()
+            original_motion = recover_from_ric(original_motion, n_joints)
+            original_motion = original_motion.view(-1, *original_motion.shape[2:]).permute(0, 2, 3, 1)
+
+        rot2xyz_pose_rep = 'xyz' if model.data_rep in ['xyz', 'hml_vec'] else model.data_rep
+        rot2xyz_mask = None if rot2xyz_pose_rep == 'xyz' else model_kwargs['y']['mask'].reshape(args.batch_size, n_frames).bool()
+        original_motion = model.rot2xyz(x=original_motion, mask=rot2xyz_mask, pose_rep=rot2xyz_pose_rep, glob=True, translation=True,
+                               jointstype='smpl', vertstrans=True, betas=None, beta=0, glob_rot=None,
+                               get_rotations_back=False)
+        original_motions.append(original_motion.cpu().numpy().copy())
+        all_lengths.append(model_kwargs['y']['lengths'].cpu().numpy())
+
+        print(f"created {len(all_motions) * args.batch_size} samples")
+
+    original_motion = np.concatenate(original_motions, axis=0)
+    all_motions = np.concatenate(all_motions, axis=0)
+    all_motions = all_motions[:total_num_samples]  # [bs, njoints, 6, seqlen]
+    all_text = all_text[:total_num_samples]
+    all_lengths = np.concatenate(all_lengths, axis=0)[:total_num_samples]
+    if 'hint' in model_kwargs['y']:
+        all_hint = np.concatenate(all_hint, axis=0)[:total_num_samples]
+        all_hint_for_vis = np.concatenate(all_hint_for_vis, axis=0)[:total_num_samples]
+    
+    if len(all_hint) != 0:
+        from utils.simple_eval import simple_eval
+        results = simple_eval(all_motions, all_hint, n_joints)
+        print(results)
+
+    if not os.path.exists(out_path):
+        os.makedirs(out_path)
+    
+    skeleton = paramUtil.kit_kinematic_chain if args.dataset == 'kit' else paramUtil.t2m_kinematic_chain
+    animation_save_path = os.path.join(out_path, "original_motion.mp4")
+    plot_3d_motion(animation_save_path, skeleton, original_motion[0].transpose(2, 0, 1), dataset=args.dataset, title="original_motion", fps=fps, hint=None)
+    input_npy_path = os.path.join(out_path, 'original_motion.npy')
+    print(f"saving original motions to [{input_npy_path}]")
+    np.save(input_npy_path,
+            {'motion': original_motion, 'text': all_text, 'lengths': all_lengths, "hint": all_hint_for_vis,
+             'num_samples': args.num_samples, 'num_repetitions': args.num_repetitions})
+
+    print(f"saving visualizations to [{out_path}]...")
+
+    npy_path = os.path.join(out_path, 'results.npy')
+    print(f"saving results file to [{npy_path}]")
+    np.save(npy_path,
+            {'motion': all_motions, 'text': all_text, 'lengths': all_lengths, "hint": all_hint_for_vis,
+             'num_samples': args.num_samples, 'num_repetitions': args.num_repetitions})
+
+
+    sample_files = []
+    num_samples_in_out_file = 7
+
+    sample_print_template, row_print_template, all_print_template, \
+    sample_file_template, row_file_template, all_file_template = construct_template_variables(args.unconstrained)
+
+    for sample_i in range(args.num_samples):
+        rep_files = []
+        for rep_i in range(args.num_repetitions):
+            caption = all_text[rep_i*args.batch_size + sample_i]
+            length = all_lengths[rep_i*args.batch_size + sample_i]
+            motion = all_motions[rep_i*args.batch_size + sample_i].transpose(2, 0, 1)[:length]
+            if 'hint' in model_kwargs['y']:
+                hint = all_hint_for_vis[rep_i*args.batch_size + sample_i]
+            else:
+                hint = None
+            save_file = sample_file_template.format(sample_i, rep_i)
+            print(sample_print_template.format(caption, sample_i, rep_i, save_file))
+            animation_save_path = os.path.join(out_path, save_file)
+            plot_3d_motion(animation_save_path, skeleton, motion, dataset=args.dataset, title=caption, fps=fps, hint=hint)
+            # Credit for visualization: https://github.com/EricGuo5513/text-to-motion
+            rep_files.append(animation_save_path)
+
+    abs_path = os.path.abspath(out_path)
+    print(f'[Done] Results are at [{abs_path}]')
+
 
 
 def save_multiple_samples(args, out_path, row_print_template, all_print_template, row_file_template, all_file_template,
